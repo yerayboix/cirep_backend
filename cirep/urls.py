97af073fb6bb211ae6c43c3cdc6506e4ff4bfdc0
aarongp11 <aarongarciapitarch@gmail.com>
2023-04-17 17:40:11 +0200

    urls de los usuarios empiezan con accounts/ y no con api/

--- cirep/urls.py
+++ cirep/urls.py
@@ -16,5 +16,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
-    path('api/', include('user.urls')),
+    path('accounts/', include('user.urls')),
 ]
