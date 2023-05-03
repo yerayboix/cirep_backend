65eb2a214e8c292d060fa0d793248867d2b93890
Yeray Boix <al376154@uji.es>
2023-05-03 22:58:36 +0200

    Estilo de colores a login actualizado y creada función logout

--- administrator/urls.py
+++ administrator/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from administrator.views.user import list_users
+from administrator.views.user import list_users, logout_view
 
 urlpatterns = [
     # Usuarios
     path('usuarios/listado', list_users, name='list_users'),
+    path('usuarios/logout', logout_view, name='logout_view'),
 ]
--- administrator/views/user.py
+++ administrator/views/user.py
@@ -1,5 +1,6 @@
+from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from user.models import User
 
@@ -9,3 +10,8 @@ def list_users(request):
     users = User.objects.all()
     context = {'users': users}
     return render(request, 'user/index.html', context)
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('login')
