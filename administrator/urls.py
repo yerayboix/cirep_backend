9c3a5ce660c10430bb5c6205d6b497248b1191da
Yeray Boix <al376154@uji.es>
2023-05-04 16:26:03 +0200

    Listado de incidencias creado.

--- administrator/urls.py
+++ administrator/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from administrator.views.reports import list_reports
 from administrator.views.user import list_users, logout_view, activate_user, desactivate_user
 
 urlpatterns = [
@@ -9,4 +10,6 @@ urlpatterns = [
     path('usuarios/logout', logout_view, name='logout_view'),
     path('usuarios/activar/<pk>', activate_user, name='activate_user'),
     path('usuarios/desactivar/<pk>', desactivate_user, name='desactivate_user'),
+    # Incidencias
+    path('incidencias/listado', list_reports, name='list_reports'),
 ]
