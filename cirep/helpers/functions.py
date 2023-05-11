534f741c1fe3734b0c632c4a5fbc419e40dcf870
Yeray Boix <al376154@uji.es>
2023-05-11 20:41:26 +0200

    Funciones para codificar y decodifiar la imagen en base64

--- cirep/helpers/functions.py
+++ cirep/helpers/functions.py
@@ -1,4 +1,8 @@
 import math
+import base64
+from PIL import Image
+from io import BytesIO
+
 
 
 # Funcion para obtener en metros la distancia entre 2 puntos especificados por coordenadas
@@ -29,3 +33,31 @@ def get_by_pk(model, pk):
     object = model.objects.get(pk=pk)
     return object
 
+
+def decodify_image(base64_string):
+    # Decodifica el string en base64 a bytes
+    imagen_bytes = base64.b64decode(base64_string)
+
+    # Crea un objeto BytesIO para leer los bytes
+    imagen_io = BytesIO(imagen_bytes)
+
+    # Abre la imagen utilizando PIL (Python Imaging Library)
+    imagen = Image.open(imagen_io)
+
+    return imagen
+
+
+def codify_image_base64(imagen):
+    # Crea un objeto BytesIO para guardar la imagen
+    buffer = BytesIO()
+
+    # Guarda la imagen en el objeto buffer en el formato deseado (por ejemplo, PNG)
+    imagen.save(buffer, format='PNG')
+
+    # Obtén los bytes de la imagen desde el buffer
+    imagen_bytes = buffer.getvalue()
+
+    # Codifica los bytes en base64
+    base64_string = base64.b64encode(imagen_bytes).decode('utf-8')
+
+    return base64_string
