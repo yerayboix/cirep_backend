2f14cb84fc1f642d585901bdf7b9cbb64933fede
Yeray Boix <al376154@uji.es>
2023-05-12 20:37:03 +0200

    Cambiado redirect en login para que lleve a dashboard

--- cirep/settings.py
+++ cirep/settings.py
@@ -132,7 +132,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/administrador/usuarios/listado'
+LOGIN_REDIRECT_URL = '/administrador/dashboard/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
